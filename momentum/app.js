ee3f2530695803b1eccd61b12d134d82652f384a
Yeonho Cho <nanyeonho@gmail.com>
2022-12-23 18:21:05 +0900

    Getting username
    1. add class="hidden" at style.css
    2. form is disappeared when its submitted
    3. h1 is appeared after #2.
    
    +  constant that only contain string usually made it upper case.
    
    + `string${const} = "string" + const

--- momentum/app.js
+++ momentum/app.js
@@ -1,16 +1,15 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
-const naverLink = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-
-function onClickNaver(event) {
-  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
-naverLink.addEventListener("click", onClickNaver);
