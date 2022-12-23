35e71d6d61c987ea9c9a35bf10faa279a1d6fbd7
Yeonho Cho <nanyeonho@gmail.com>
2022-12-23 17:57:44 +0900

    Events part Two
    1. EventListner 는 event 실행 시 브라우저가 함수를 실행한다.
    
    2. 함수 실행 시 인자에 event 에 대한 정보도 담아준다.
    
    3. 우리는 자리만 만들어 주면 된다. 이 정보 안에는 몇 가지 함수도 들어있다.

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
+const naverLink = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function onClickNaver(event) {
+  event.preventDefault();
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+
+naverLink.addEventListener("click", onClickNaver);
